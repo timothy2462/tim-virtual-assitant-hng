4e89731c538635c941bc7b846c83f9a93d436229
BaZz <bazuaye50@gmail.com>
2023-10-01 05:40:00 +0100

    ball

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,18 @@ function App() {
 	const left = useRef(null);
 	const [isResetScheduled, setIsResetScheduled] =
 		useState(false);
+	const [popup, setPopup] = useState(false);
+	//
 	const [ballPosition, setBallPosition] = useState<{
 		x: number;
 		y: number;
 	}>({ x: 0, y: 0 });
+
+	// Reset the ball position
 	const initialX = window.innerWidth - 200;
 	const initialY = window.innerHeight - 200;
+
+	//
 	const handleClick = (
 		e: React.MouseEvent<HTMLButtonElement>,
 	) => {
@@ -22,20 +28,21 @@ function App() {
 		const clampedY = Math.max(0, Math.min(initialY, targetY));
 
 		setBallPosition({ x: clampedX, y: clampedY });
-
 		setIsResetScheduled(true);
+		setTimeout(() => {
+			setPopup(true);
+		}, 800);
 		// Schedule the reset after 40 seconds (40,000 milliseconds)
 		setTimeout(() => {
 			setBallPosition({ x: 0, y: 0 });
 			setIsResetScheduled(false);
+			setPopup(false);
 		}, 10000);
 	};
 
 	return (
 		<>
 			<div className="App">
-				<h2>Hello World</h2>
-
 				<div
 					className="ball"
 					onTransitionEndCapture={() => {
@@ -47,6 +54,7 @@ function App() {
 						}px, ${ballPosition.y ? ballPosition.y : initialY}px)`,
 					}}>
 					<h3>ProFile</h3>
+					{popup && <p>Message</p>}
 				</div>
 				<div className="btns">
 					<button
